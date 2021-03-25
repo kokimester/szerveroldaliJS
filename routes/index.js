dfd0fd11a5b3aee726074a4daedeb303a1d8e6a5
Fejes Daniel <fejesdani@gmail.com>
2021-03-25 22:10:01 +0100

    added routes

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,10 @@ module.exports = function(app) {
         renderMW(objRepo,'profil'));
     app.use('/hirdetes',
         renderMW(objRepo,'hirdeteseim'));
+    app.use('/login',
+        renderMW(objRepo,'login'));
+    app.use('/register',
+        renderMW(objRepo,'regisztracio'));
+    app.use('/newpassword',
+        renderMW(objRepo,'elfelejtett'));
 };
