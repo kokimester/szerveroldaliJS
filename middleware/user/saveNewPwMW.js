e9b5d908ed60402f2ca584fc77ee83b80b5343d2
Fejes Daniel <fejesdani@gmail.com>
2021-04-09 10:53:19 +0200

    moved req.body check to savenewuserMW

--- middleware/user/saveNewPwMW.js
+++ middleware/user/saveNewPwMW.js
@@ -4,29 +4,11 @@ var requireOption = require('../common').requireOption;
 module.exports = function (objectrepository) {
     return function(req, res, next) {
 
-        if(
-            (typeof req.body.inputOldPassword === 'undefined') ||
-            (typeof req.body.inputNewPassword1 === 'undefined') ||
-            (typeof req.body.inputNewPassword2 === 'undefined')
-        )
-        {
-            return next();
-        }
-        if(
-            (req.body.inputOldPassword === '') ||
-            (req.body.inputNewPassword1 === '') ||
-            (req.body.inputNewPassword2 === '')
-        )
-        {
-            console.log('hulyegyerek toltsd ki a mezoket')
-            return next();
-        }
+        console.log('-----------saveNewPwMW:-----------');
         
         
-
-
-        console.log('saving new password');
-        console.log(req.body);
+        console.log('saving new password:');
+        console.log(res.locals.newPassword);
         next();
     }
 }
